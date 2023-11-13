615a69897248eb6d7961a3c11baf9b4c00864546
Ebenezer <ebenirene5@gmail.com>
2023-11-14 00:31:38 +0100

    added refresh configurations at runtime using actuator path

--- v2 springCloud/account-service/src/main/java/com/kokoro/accountservice/dto/AccountsContactInfoDto.java
+++ v2 springCloud/account-service/src/main/java/com/kokoro/accountservice/dto/AccountsContactInfoDto.java
@@ -1,10 +1,19 @@
 package com.kokoro.accountservice.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.*;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
+
 }
